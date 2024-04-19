209c43557b19914e14a420af582448640d60a96b
Omar-Algassim <omarggg2@gamil.com>
2024-04-20 01:01:45 +0200

    update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,6 +12,8 @@ app = Flask(__name__)
 def hello_hbnb():
     return "Hello HBNB!"
 
-
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
 if "__main__" == __name__:
     app.run(port=5000, host="0.0.0.0")
