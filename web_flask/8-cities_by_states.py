1fcf71d26d94a0cae5af152242f2522de8422cef
Omar-Algassim <omarggg2@gamil.com>
2024-04-22 03:35:46 +0200

    cities in states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""Flask app """
+
+
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_text(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def py_text(n):
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def tamplate_number(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def even_or_odd(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+@app.teardown_appcontext
+def teardown_clear(exec):
+    """clear the sqlalchemy"""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    state_list = storage.all(State)
+    return render_template("7-states_list.html", state_list=state_list)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_state():
+    state_list = storage.all(State)
+    return render_template("7-states_list.html", state_list=state_list)
+
+
+if "__main__" == __name__:
+    app.run(port=5000, host="0.0.0.0", debug=True)
